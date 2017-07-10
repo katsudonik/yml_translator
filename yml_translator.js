a9c513f72f620c297274fa7f0fc98b8aa7a7fea5
kaikeda <kaikeda@valuecommerce.co.jp>
2017-07-10 15:35:30 +0900

    .

---
+++ yml_translator.js
@@ -0,0 +1,18 @@
+var casper = require("casper").create({
+    verbose: true,
+    logLevel: 'debug',
+    pageSettings: {
+        loadImages:  true,
+        loadPlugins: false,
+        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'
+    }
+});
+
+// affiliate amazon top
+casper.start('https://translate.google.co.jp/', function() {
+    var ja = '‘—‚é';
+    this.sendKeys('#source', ja, {reset: true});
+    this.echo(this.getHTML('#result_box'));
+});
+
+casper.run();
